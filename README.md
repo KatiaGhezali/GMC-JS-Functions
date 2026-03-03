0a76bbfbc705b2e9b85f32923f87be14dfc148ab
Katia Ghezali <ghezalikatia@hotmail.com>
2026-03-03 03:30:00 +0100

    JS Functions

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # GMC-JS-Functions
+
+This repository contains a set of javascript functions for string manipulations, array operations, and basic mathematical calculations.
+
+## String Manipulation Functions:
+
+1.  **Reverse a String:** Reverses a given string.
+2.  **Count Characters:** Counts the number of characters in a string.
+3.  **Capitalize Words:** capitalizes the first letter of each word in a sentence.
+
+## Array Functions:
+
+4.  **Find Maximum and Minimum:**Finds the maximum and minimum values in an array of numbers.
+5.  **Sum of Array:** Calculates the sum of all elements in an array.
+6.  **Filter Array:** Filters out elements from an array based on a given condition.
+
+## Mathematical Functions:
+
+7. **Factorial:** Calculates the factorial of a given number.
+8. **Prime Number Check:** Checks if a number is prime or not.
+9. **Fibonacci Sequence:** Generates the Fibonacci sequence up to a given number of terms.
+
+## Outputs:
+
+![Results](result.png)
---
+++ index.js
@@ -0,0 +1,123 @@
+// String Manipulation Functions
+
+// 1- Reverse a string
+
+function reverseStr(str) {
+  return str.split("").reverse().join("");
+}
+
+// 2- Count characters
+
+function countChar(str) {
+  return str.length;
+}
+
+// 3- Capitalize words
+
+function capitalizeWords(sentence) {
+  let words = sentence.split(" ");
+  let result = "";
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    result = result + word[0].toUpperCase() + word.slice(1);
+
+    if (i < words.length - 1) {
+      result = result + " ";
+    }
+  }
+  return result;
+}
+
+// Array Functions:
+
+// 1- Find Maximum and Minimum
+
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+// 2- Sum of Array
+
+function sum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum;
+}
+
+// 3- Filter Array
+
+function filterArr(arr, condition) {
+  return arr.filter(condition);
+}
+
+// Mathematical Functions:
+
+// 1- Factorial
+
+function factorial(n) {
+  let result = 1;
+
+  for (let i = 2; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
+}
+
+// 2- Prime Number Check
+
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// 3- Fibonacci Sequence
+
+function Fibonacci(n) {
+  let sequence = [];
+
+  if (n <= 0) {
+    return sequence;
+  }
+  if (n === 1) {
+    return [0];
+  }
+
+  sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    sequence.push(sequence[i - 1] + sequence[i - 2]);
+  }
+
+  return sequence;
+}
+
+// TESTS:
+
+console.log(reverseStr("hello")); // olleh
+console.log(countChar("hello")); // 5
+console.log(capitalizeWords("hello world")); // "Hello World"
+
+console.log(findMax([2, 5, 6, 9, 0])); // 5
+console.log(sum([1, 5, 7])); //10
+console.log(filterArr([1, 5, 3, 6], (x) => x > 3)); // [5,6]
+
+console.log(factorial(5)); // 120
+console.log(isPrime(7)); // true
+console.log(Fibonacci(7)); // [0,1,1,2,3,5,8]
